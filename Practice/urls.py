49337e9d256b9b733d7652733d263bc5f8119421
Rafael Salimov <raphasalimov@gmail.com>
2021-07-31 13:07:09 +0400

    setting up new app for aggregations

--- Practice/urls.py
+++ Practice/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('', include('agg.urls')),
 ]
---
+++ agg/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AggConfig(AppConfig):
+    name = 'agg'
